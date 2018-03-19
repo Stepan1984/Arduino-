62d0134536a5224ea403ee202ddb772c18f12da6
Banana Android Z <ndrdbnnz@gmail.com>
2018-03-19 21:02:53 +0300

    arduino 3

--- app/src/main/java/com/banana/arduinocontroller/MainActivity.java
+++ app/src/main/java/com/banana/arduinocontroller/MainActivity.java
@@ -1,16 +1,20 @@
 package com.banana.arduinocontroller;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.Preference;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
 Button settings;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        updateTheme();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.controller);
         settings = findViewById(R.id.settings);
@@ -21,6 +25,37 @@ Button settings;
                 startActivity(intent);
             }
         });
+        subscribeSharedPreferenceChanges();
         }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unsubscribeSharedPreferenceChanges();
+    }
+
+    private void updateTheme(){
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        String theme = preferences.getString("pref_theme","Light");
+        if(theme.equals("Light")){
+            setTheme(R.style.AppTheme);
+        } else {
+            setTheme(R.style.AppTheme_Dark);
+        }
+
+    }
+    private void subscribeSharedPreferenceChanges(){
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        preferences.registerOnSharedPreferenceChangeListener(this);
+    }
+    private void unsubscribeSharedPreferenceChanges(){
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        preferences.unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
+        if (s.equals("pref_theme")){
+            recreate();}
+    }
 }
--- app/src/main/java/com/banana/arduinocontroller/Main_Activity_Controller.java
+++ app/src/main/java/com/banana/arduinocontroller/Main_Activity_Controller.java
@@ -2,10 +2,14 @@ package com.banana.arduinocontroller;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
 
 public class Main_Activity_Controller extends PreferenceActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
 
@@ -13,19 +17,32 @@ public class Main_Activity_Controller extends PreferenceActivity implements Shar
     protected void onCreate(Bundle savedInstanceState) {
         updateTheme();
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
         addPreferencesFromResource(R.xml.settings);
+        Toolbar toolbar = findViewById(R.id.Toolbar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+                public void onClick(View v){
+                    finish();
+                }
+        });
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
         preferences.registerOnSharedPreferenceChangeListener(this);
+
+        updateBluetoothAddressSummary();
+        updateThemeSummary();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
+
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
         preferences.unregisterOnSharedPreferenceChangeListener(this);
     }
@@ -38,12 +55,28 @@ public class Main_Activity_Controller extends PreferenceActivity implements Shar
         } else {
             setTheme(R.style.AppTheme_Dark);
         }
+
     }
 
     @Override
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
         if (s.equals("pref_theme")){
             recreate();
+        } else if (s.equals("pref_address")) {
+            updateBluetoothAddressSummary();
         }
+    }
+
+    private void updateBluetoothAddressSummary(){
+     Preference preference =  findPreference("pref_address");
+     SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+     String value = preferences.getString("pref_address", "ДЕФОЛТ");
+     preference.setSummary(value);
+    }
+    private void updateThemeSummary(){
+        Preference preference = findPreference("pref_theme");
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        String value = preferences.getString("pref_theme","default");
+        preference.setSummary(value);
     }
 }
